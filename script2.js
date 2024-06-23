84a8f3229c1dc0ea3b7511894a1fa0ebfad2566e
testsgithubsrit <nehagarhwa5@gmail.com>
2024-06-23 17:27:19 +0530

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,5 +1,28 @@
-var typed = new Typed('#element', {
+   var typed = new Typed('#element', {
     strings: ['front end', 'web developer.'],
     typeSpeed: 70,
+  });
+  let currentSlide = 0;
+  function showSlide(index){
+    const slides =document.querySelectorAll('.slide');
+    if(index >= slides.length){
+      currentSlide = 0;
+    }
+    else if (index < 0){
+currentSlide = slides.length - 1;
+    }
+    else{
+      currentSlide=index;
+    }
+    const offset = - currentSlide*100;
+    document.querySelector('.slides').style.transform=
+    'translateX(${offset}%)';
+
+  }
+  function moveSlide(step) {
+    showSlide(currentSlide + step);
+
+  }document.addEventListener('DOMContentLoaded',()=>{
+    showSlide(currentSlide);
   });
   
